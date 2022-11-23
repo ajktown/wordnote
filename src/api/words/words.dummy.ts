d22616eba49a82cd118a2ac883d0f0f73840b683
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-11-23 12:12:59 +0900

    feat: dummy data

--- src/api/words/words.dummy.ts
+++ src/api/words/words.dummy.ts
@@ -3,16 +3,16 @@ import { WordData } from "./words.interface"
 export const DUMMY_WORDS: WordData[] = [
   {
     id: "2ce0dc45-4542-443b-9d20-f15f5d8c65e8",
-    term: "Hello",
-    pronunciation: "",
-    definition: "",
+    term: "breadth",
+    pronunciation: "breth",
+    definition: "폭 (Breadth First Search, BFS) (broad)",
     example: "",
   },
   {
     id: "2ce0dc45-4542-443b-9d20-f15f5d8c65e9",
-    term: "World",
+    term: "top",
     pronunciation: "",
-    definition: "",
-    example: "",
+    definition: "(can be also)favorite",
+    example: "Select your top colors, and we will send the samples!",
   }
 ]
