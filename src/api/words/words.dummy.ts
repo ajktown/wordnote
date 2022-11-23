5af6a7fc24d138d0d11362e0adff35a3edd3806c
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-11-23 12:57:35 +0900

    fix: slight note

--- src/api/words/words.dummy.ts
+++ src/api/words/words.dummy.ts
@@ -12,7 +12,7 @@ export const DUMMY_WORDS: WordData[] = [
     id: "2ce0dc45-4542-443b-9d20-f15f5d8c65e9",
     term: "top",
     pronunciation: "",
-    definition: "(can be also)favorite",
+    definition: "(can be also) favorite",
     example: "Select your top colors, and we will send the samples!",
   }
 ]
