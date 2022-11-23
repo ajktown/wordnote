cf9e8eb6bf341d47f3a60708170ed614df2f1a71
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-11-23 12:46:34 +0900

    feat: appbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
 import { FC } from 'react'
+import Appbar from '../components/organism_appbar'
 import HomeLayout from '../layouts'
-import StyledAppbarMolecule from '../molecules/StyledAppbar.m'
 
 const Home: FC = () => {
   return (
-    <StyledAppbarMolecule
-      title="Wordnote"
-      titleLogoPath="/favicon_archived/android-chrome-512x512.png"
-    >
+    <Appbar>
       <HomeLayout />
-    </StyledAppbarMolecule>
+    </Appbar>
   )
 }
 
