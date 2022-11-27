30cfecfc7cfe122129486dfc58df88f909f59215
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-11-27 14:41:54 +0900

    feat: modify is favorite with recoil

--- src/components/atom_word_card_favorite_icon/index.tsx
+++ src/components/atom_word_card_favorite_icon/index.tsx
@@ -3,19 +3,37 @@ import StyledIconButtonAtom from '@/atoms/StyledIconButton'
 import { FC } from 'react'
 import FavoriteWordIcon from '@mui/icons-material/FavoriteTwoTone'
 import NotFavoriteWordIcon from '@mui/icons-material/FavoriteBorderTwoTone'
+import { useRecoilState } from 'recoil'
+import { wordsState } from '@/recoils/state_atoms/words.state'
 
 interface Props {
   word: WordData
 }
 const WordCardFavoriteIcon: FC<Props> = ({ word }) => {
+  const [words, setWords] = useRecoilState(wordsState)
+  
+  const handleClickFavoriteIcon = () => {
+    const foundIndex = words.findIndex(el => el.id === word.id)
+    if (foundIndex === -1) return
+
+    const modifyingWord = words[foundIndex]
+
+    const copiedWords = [...words]
+    copiedWords.splice(foundIndex, 1, {
+      ...modifyingWord,
+      isFavorite: !modifyingWord.isFavorite,
+    })
+    setWords(copiedWords)
+  }
   if (word.isFavorite)
     return (
       <StyledIconButtonAtom
-        jsxElementButton={<FavoriteWordIcon style={{ color: `FF0000` }} />}
+      handleClick={() => handleClickFavoriteIcon()}
+        jsxElementButton={<FavoriteWordIcon style={{ color: `FF0000` /* Red */ }} />}
       />
     )
 
-  return <StyledIconButtonAtom jsxElementButton={<NotFavoriteWordIcon />} />
+  return <StyledIconButtonAtom handleClick={() => handleClickFavoriteIcon()} jsxElementButton={<NotFavoriteWordIcon />} />
 }
 
 export default WordCardFavoriteIcon
