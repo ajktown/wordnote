97568ee570f86e13ba115ab0e11a726079154ec4
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-11-29 09:16:58 +0900

    feat: simple logger for get word by id api

--- src/api/words/get-word-by-id.api.ts
+++ src/api/words/get-word-by-id.api.ts
@@ -4,6 +4,7 @@ import { DUMMY_WORDS } from "./words.dummy"
 import { WordData } from "./words.interface"
 
 export const getWordByIdApi = async (wordId: string): Promise<WordData> => {
+  console.log("getting word... for word with id: " + wordId)
   const foundIndex = DUMMY_WORDS.findIndex(el => el.id === wordId)
   if (foundIndex === -1) throw new Error()
 
