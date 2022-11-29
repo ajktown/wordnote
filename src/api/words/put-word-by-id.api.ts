d7be91b2db397f6684287d8d367d8d7f194c2aa5
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-11-29 09:19:24 +0900

    fix: simple logger for put word by id api

--- src/api/words/put-word-by-id.api.ts
+++ src/api/words/put-word-by-id.api.ts
@@ -6,5 +6,5 @@ export const putWordByIdApi = async (
   wordId: string,
   modifying: Partial<Omit<WordData, 'id'>>,
 ): Promise<void> => {
-  console.log(`Putting the word: ` + wordId + `with: ${JSON.stringify(modifying)}`)
+  console.log(`Putting the word: ` + wordId + ` with: ${JSON.stringify(modifying)}`)
 }
