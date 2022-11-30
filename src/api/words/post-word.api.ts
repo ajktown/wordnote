2a170b9cfd3ce0111101c563ca9f31209cad5cac
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-12-01 08:15:25 +0900

    feat: post word api returns word data created

--- src/api/words/post-word.api.ts
+++ src/api/words/post-word.api.ts
@@ -2,6 +2,8 @@
 
 import { WordData } from './words.interface'
 
-export const postWordApi = async (newWord: WordData): Promise<void> => {
+export const postWordApi = async (newWord: WordData): Promise<WordData> => {
   console.log(`Posting the word...: ` + JSON.stringify(newWord))
+
+  return newWord
 }
