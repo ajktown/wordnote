f298aade409382ffe7c44b16dfe7b144189d46b3
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-12-04 15:39:37 +0900

    feat: styled circular progress

---
+++ src/atoms/StyledCiruclarLoading.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+
+const StyledCircularLoading: FC = () => {
+  return <CircularProgress />
+}
+
+export default StyledCircularLoading
