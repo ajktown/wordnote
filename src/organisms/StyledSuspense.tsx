9e5797e3d1d68c6364c68297d6e8f59e6f1ad79e
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-12-06 08:32:02 +0900

    feat: styled suspense

---
+++ src/organisms/StyledSuspense.tsx
@@ -0,0 +1,18 @@
+import { FC, Suspense } from 'react'
+
+interface Props {
+  children?: JSX.Element[] | JSX.Element
+}
+
+// This is a legit troll component, but I love it.
+const PrivateStyledNull: FC = () => null
+
+const StyledSuspense: FC<Props> = ({ children }) => {
+  return (
+    <Suspense fallback={<PrivateStyledNull />}>
+      {children}
+    </Suspense>
+  )
+}
+
+export default StyledSuspense
