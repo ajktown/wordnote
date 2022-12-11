9ef04085004817277366203e659fb9392abb92b4
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-12-12 07:55:43 +0900

    comment: styled circular loading

--- src/atoms/StyledCircularLoading.tsx
+++ src/atoms/StyledCircularLoading.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
 import CircularProgress from '@mui/material/CircularProgress'
 
+// TODO: This will be eventually styled with the app theme
+
 const StyledCircularLoading: FC = () => {
   return <CircularProgress />
 }
