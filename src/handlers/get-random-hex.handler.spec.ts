56810124130d8522ac4764d68d4c5f643476d33a
Aaron Jeongwoo Kim <53258958+officialajkim@users.noreply.github.com>
2022-12-11 14:32:00 +0900

    feat: Jest Unit Test & Apply for first handler (#4)
    TID;

---
+++ src/handlers/get-random-hex.handler.spec.ts
@@ -0,0 +1,46 @@
+import { getRandomHexHandler } from './get-random-hex.handler'
+// TODO: Implement the following
+// TODO: 1. the Auto generation tool for TypeScript
+// TODO: 1. The Coverage Test for the TypeScript
+// TODO: 1. If can't be found, you may follow the exact same format as the Go supported auto generation tool (format)
+
+describe(`getRandomHexHandler`, () => {
+  it(`should expose a function`, () => {
+    expect(getRandomHexHandler).toBeDefined()
+  })
+
+  const DEFAULT_HEX_LENGTH = 6
+  interface Test {
+    hexLength?: number
+    wantLength: number
+  }
+
+  const tests: Test[] = [
+    {
+      hexLength: undefined,
+      wantLength: DEFAULT_HEX_LENGTH,
+    },
+    {
+      hexLength: 1,
+      wantLength: 1,
+    },
+    {
+      hexLength: 2,
+      wantLength: 2,
+    },
+    {
+      hexLength: 6,
+      wantLength: 6,
+    },
+    {
+      hexLength: 10,
+      wantLength: 10,
+    },
+  ]
+
+  it(`getRandomHexHandler should return the expected output with correct length`, () => {
+    tests.forEach((test) => {
+      expect(getRandomHexHandler(test.hexLength).length).toBe(test.wantLength)
+    })
+  })
+})
