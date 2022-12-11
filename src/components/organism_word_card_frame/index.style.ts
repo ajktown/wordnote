af4f53612a79a7308afb7d241707717d3e55b85e
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-12-11 14:06:11 +0900

    refactor: style code as a file

---
+++ src/components/organism_word_card_frame/index.style.ts
@@ -0,0 +1,10 @@
+
+
+export const WordCardFrameStyle = {
+  p: 1,
+  style: {backgroundColor: `#aaa` },
+  width: "100%",
+  maxWidth: 1000,
+  spacing: 1,
+  borderRadius: 4,
+}
--- src/components/organism_word_card_frame/index.tsx
+++ src/components/organism_word_card_frame/index.tsx
@@ -3,18 +3,12 @@ import { Stack, Box } from '@mui/material'
 import NewWordBox from '../molecule_new_word_box'
 import WordCardsFrameRefreshButton from '../atom_word_cards_frame_refresh_button'
 import WordCardsChunk from '../organism_word_card_chunk'
+import { WordCardFrameStyle } from './index.style'
 
 const WordCardFrame: FC = () => {
   return (
     <Stack width="100%" alignItems="center">
-      <Stack
-        p={1}
-        style={{ backgroundColor: `#aaa` }}
-        width="100%"
-        maxWidth={1000}
-        spacing={1}
-        borderRadius={4}
-      >
+      <Stack {...WordCardFrameStyle} >
         {/* Header */}
         <Stack direction="row" spacing={0.5}>
           <Box flexGrow={1} />
