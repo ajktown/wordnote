bfd90ddc794b2f7a08e463546697d29b861d4285
Jeongwoo Kim <jkim67cloud@gmail.com>
2022-12-31 14:45:49 +0900

    feat: country flag with language

--- src/components/atom_language_selector/index.dummy.ts
+++ src/components/atom_language_selector/index.dummy.ts
@@ -1,24 +1,28 @@
 import ISO6391, { LanguageCode } from 'iso-639-1'
+import getUnicodeFlagIcon from 'country-flag-icons/unicode'
+import { GlobalCountryIsoCode } from '@/global.interface'
 
 // TODO: At this point I am selecting only certain languages available, and will be extended later in the future.
 
-const wordnoteAvailableLanguageCodes: LanguageCode[] = [
-  `ko`,
-  `en`,
-  `zh`,
-  `ja`,
-  `fr`,
+const wordnoteAvailableLanguageCodeAndCountries: [LanguageCode, GlobalCountryIsoCode][] = [
+  [`ko`, GlobalCountryIsoCode.KoreaRepublicOf],
+  [`en`, GlobalCountryIsoCode.UnitedStates],
+  [`zh`, GlobalCountryIsoCode.China],
+  [`ja`, GlobalCountryIsoCode.Japan],
+  [`fr`, GlobalCountryIsoCode.France],
 ]
 
 interface AvailableLanguage {
   code: LanguageCode
   name: string
   nativeName: string
+  flagUnicode: string
 }
 
 export const PUBLIC_STATIC_AVAILABLE_LANGUAGES: AvailableLanguage[] =
-  wordnoteAvailableLanguageCodes.map((languageCode) => ({
+  wordnoteAvailableLanguageCodeAndCountries.map(([languageCode, countryCode]) => ({
     code: languageCode,
     name: ISO6391.getName(languageCode),
     nativeName: ISO6391.getNativeName(languageCode),
+    flagUnicode: getUnicodeFlagIcon(countryCode),
   }))
--- src/components/atom_language_selector/index.tsx
+++ src/components/atom_language_selector/index.tsx
@@ -29,7 +29,7 @@ const LanguageSelector: FC<Props> = ({ languageCode, onClickModify }) => {
       <StyledDropDown
         items={PUBLIC_STATIC_AVAILABLE_LANGUAGES.map((lang) => ({
           id: lang.code,
-          title: lang.nativeName,
+          title: lang.nativeName + lang.flagUnicode, 
         }))}
         selectedId={selectedId}
         onChange={handleChange}
