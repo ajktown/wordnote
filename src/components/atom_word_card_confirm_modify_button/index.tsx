78a281e18c9120d9b56f5a575088a8ccf6dc5b77
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-01-01 11:43:03 +0900

    feat: you can modify now with modify button

--- src/components/atom_word_card_confirm_modify_button/index.tsx
+++ src/components/atom_word_card_confirm_modify_button/index.tsx
@@ -1,12 +1,30 @@
 import StyledTextButtonAtom from '@/atoms/StyledTextButton'
-import { FC } from 'react'
-
+import { FC, useCallback } from 'react'
+import {  useRecoilValue, useResetRecoilState } from 'recoil'
+import { wordsFamily, selectedWordForDialogState } from '@/recoil/words.state'
+import { isDeeplySameObjectsHandler } from '@/handlers/is-deeply-same-objects.handler'
+import { usePutWord } from '@/hooks/words/use-put-word.hook'
 interface Props {
   wordId: string
 }
+
 const WordCardConfirmModifyButton: FC<Props> = ({ wordId }) => {
-  console.log(wordId) // TODO: Implement
-  return <StyledTextButtonAtom title="Modify" variant="text" />
+  const word = useRecoilValue(wordsFamily(wordId))
+  const selectedWordForDialog = useRecoilValue(selectedWordForDialogState)
+  const putWord = usePutWord(wordId)
+  const resetDialog = useResetRecoilState(selectedWordForDialogState)
+
+  const handleClickChange = useCallback(async () => {
+    if (!selectedWordForDialog) return
+
+    await putWord(selectedWordForDialog)
+    resetDialog()
+  }, [selectedWordForDialog, putWord, resetDialog])
+
+  if (!selectedWordForDialog || !word) return null
+  if (isDeeplySameObjectsHandler(word, selectedWordForDialog)) return null
+  
+  return <StyledTextButtonAtom title="Modify" variant="text" onClick={handleClickChange}/>
 }
 
 export default WordCardConfirmModifyButton
