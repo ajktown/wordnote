2a328d6ae89dab8d9eed93a9bbda14f2127f9cce
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-01-01 14:53:26 +0900

    feat: use warning hook

--- src/components/molecule_word_card/index.dialog.tsx
+++ src/components/molecule_word_card/index.dialog.tsx
@@ -4,6 +4,7 @@ import { useRecoilValue, useResetRecoilState } from 'recoil'
 import WordCard from '.'
 import { selectedWordForDialogState } from '@/recoil/words.state'
 import { usePutWordCache } from '@/hooks/words/use-put-word-cache.hook'
+import { useWarning } from '@/hooks/use-warning.hook'
 
 const WordCardDialog: FC = () => {
   const selectedWordId = useRecoilValue(selectedWordForDialogState)
@@ -15,11 +16,14 @@ const WordCardDialog: FC = () => {
     resetSelectedWordForDialog()
   }, [resetSelectedWordForDialog, resetModify])
 
+  const [warningDialog, handleClick] = useWarning(handleCloseDialog)
+
   if (!selectedWordId) return null
 
   return (
-    <StyledDialog onClose={handleCloseDialog}>
+    <StyledDialog onClose={handleClick}>
       <WordCard wordId={selectedWordId} editingMode />
+      {warningDialog}
     </StyledDialog>
   )
 }
---
+++ src/components/organism_warning_dialog/index.tsx
@@ -0,0 +1,32 @@
+import StyledTextButtonAtom from '@/atoms/StyledTextButton'
+import StyledDialog from '@/organisms/StyledDialog'
+import { Typography } from '@mui/material'
+import { FC } from 'react'
+
+interface Props {
+  open: boolean
+  onClickCancel?: () => any
+  onClickConfirm?: () => any
+}
+// TODO: Design this component
+const WarningDialog: FC<Props> = ({ open, onClickCancel,  onClickConfirm }) => {
+  if (!open) return null
+
+  return (
+    <StyledDialog>
+      <Typography>
+        {"Are you sure?"}
+      </Typography>
+      <StyledTextButtonAtom 
+        title="Cancel"
+        onClick={onClickCancel}
+      />
+      <StyledTextButtonAtom
+        title="Confirm"
+        onClick={onClickConfirm}
+      />
+    </StyledDialog>
+  )
+}
+
+export default WarningDialog
