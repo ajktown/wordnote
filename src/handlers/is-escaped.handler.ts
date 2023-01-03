759884a8b874cafd24454f3a61df3248c0ae4d97
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-01-03 12:42:40 +0900

    feat: is escaped handler

--- src/handlers/is-escaped.handler.ts
+++ src/handlers/is-escaped.handler.ts
@@ -5,7 +5,7 @@ type Escapers = string | string[]
 // TODO: This requires test
 // TODO: I am not sure yet this handler works properly yet.
 export const isEscapedHandler = (given: string, escapers: Escapers): boolean => {
-  if (given.length < 2) return false
+  if (given.length < 1) return false
 
   const escaperArr: string[] = Array.isArray(escapers) ? escapers : [escapers]
   return escaperArr.includes(given[0])
