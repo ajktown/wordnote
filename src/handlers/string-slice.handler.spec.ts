8ab384d51b3e7fe3c0d122a9f82fb9f3f2f7a4f8
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-01-08 07:23:52 +0900

    fix: test sentence

--- src/handlers/string-slice.handler.spec.ts
+++ src/handlers/string-slice.handler.spec.ts
@@ -54,20 +54,20 @@ describe(`stringSliceHandler`, () => {
       wantSlicedFront: `new`,
       wantSlicedRear: `hello:world`,
     },
-    {
+    { // TODO: This is failing
       sampleString: `new:hello$$:world`,
       wantSlicedFront: `new`,
       wantSlicedRear: `hello:world$`,
     },
   ]
 
-  it(`stringSliceHandler should return the expected output`, () => {
-    tests.forEach((test) => {
-      const [slicedFront, slicedRear] = stringSliceHandler(
-        test.sampleString,
-        PRIVATE_FINAL_SLICE_WITH,
-        PRIVATE_FINAL_ESCAPE_CHAR,
-      )
+  tests.forEach((test) => {
+    const [slicedFront, slicedRear] = stringSliceHandler(
+      test.sampleString,
+      PRIVATE_FINAL_SLICE_WITH,
+      PRIVATE_FINAL_ESCAPE_CHAR,
+    )
+    it(`stringSliceHandler should return the expected output from "${test.sampleString}"`, () => {
       expect(slicedFront).toBe(test.wantSlicedFront)
       expect(slicedRear).toBe(test.wantSlicedRear)
     })
