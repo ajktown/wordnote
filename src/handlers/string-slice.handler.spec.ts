841f269c9e98b51f4bdb7de8eb538ad6e4f656d1
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-01-08 11:43:41 +0900

    feat: another test and finalize the string slicer handler

--- src/handlers/string-slice.handler.spec.ts
+++ src/handlers/string-slice.handler.spec.ts
@@ -1,8 +1,4 @@
 import { stringSliceHandler } from './string-slicer.handler'
-// TODO: Implement the following
-// TODO: 1. the Auto generation tool for TypeScript
-// TODO: 1. The Coverage Test for the TypeScript
-// TODO: 1. If can't be found, you may follow the exact same format as the Go supported auto generation tool (format)
 
 const PRIVATE_FINAL_ESCAPE_CHAR = `$`
 const PRIVATE_FINAL_SLICE_WITH = `:`
@@ -58,6 +54,11 @@ describe(`stringSliceHandler`, () => {
       sampleString: `new:hello$$:world`,
       wantSlicedFront: `new`,
       wantSlicedRear: `hello$:world`,
+    },
+    {
+      sampleString: `new$:hello$$:world`,
+      wantSlicedFront: `new:hello$:world`,
+      wantSlicedRear: ``,
     },
   ]
 
