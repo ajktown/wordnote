e86b68e9d94eccde17218acdfaffe441a547a66a
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-01-09 12:36:55 +0900

    fix: typo for the api calls

--- src/api/semesters/get-semesters.api.ts
+++ src/api/semesters/get-semesters.api.ts
@@ -3,7 +3,7 @@
 import { DUMMY_SEMESTERS } from './index.dummy'
 import { SemesterData } from './index.interface'
 
-export const getWordsApi = async (): Promise<SemesterData[]> => {
+export const getSemestersApi = async (): Promise<SemesterData[]> => {
   console.log(`Getting semesters from server...`)
   return DUMMY_SEMESTERS
 }
--- src/recoil/semesters.state.ts
+++ src/recoil/semesters.state.ts
@@ -5,12 +5,12 @@ enum PrivateSemesterRecoilKey {
   SemesterSelected = `SemesterSelected`,
 }
 
-export const semestersState = atom<string[]>({
+export const semestersState = atom<number[]>({
   key: PrivateSemesterRecoilKey.Semesters,
   default: [],
 })
 
-export const selectedSemester = atom<null | string>({
+export const selectedSemester = atom<null | number>({
   key: PrivateSemesterRecoilKey.SemesterSelected,
   default: null,
 })
