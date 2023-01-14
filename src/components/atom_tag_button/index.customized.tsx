7b304079e653e6af636714b9f1a06d67da0ff2d5
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-01-14 18:20:55 +0900

    feat: normal tags are selectable too

--- src/components/atom_tag_button/index.customized.tsx
+++ src/components/atom_tag_button/index.customized.tsx
@@ -1,11 +1,34 @@
 import StyledTagButtonAtom from '@/atoms/StyledTagButton'
-import { FC } from 'react'
+import { GlobalMuiTagVariant } from '@/global.interface'
+import { selectedTagFamily } from '@/recoil/tags.state'
+import { FC, useCallback, useMemo } from 'react'
+import { useRecoilState } from 'recoil'
 
 interface Props {
   label: string
 }
 const TagButtonCustomized: FC<Props> = ({ label }) => {
-  return <StyledTagButtonAtom label={`#` + label} />
+  const [isTagSelected, setTagSelected] = useRecoilState(
+    selectedTagFamily(label),
+  )
+
+  const variant: GlobalMuiTagVariant = useMemo(() => {
+    return isTagSelected ? `filled` : `outlined`
+  }, [isTagSelected])
+
+  const handleClick = useCallback(() => {
+    setTagSelected(!isTagSelected)
+  }, [isTagSelected, setTagSelected])
+
+  return (
+    <StyledTagButtonAtom
+      label={`#` + label}
+      style={{
+        variant,
+      }}
+      onClick={handleClick}
+    />
+  )
 }
 
 export default TagButtonCustomized
