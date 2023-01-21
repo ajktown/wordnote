c2edacf53b85432ab1d124ded605675ef4a2a805
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-01-21 19:30:39 +0900

    fix: message

--- src/components/organism_word_card_chunk/index.no_words_found.tsx
+++ src/components/organism_word_card_chunk/index.no_words_found.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react'
 import { useRecoilValue } from 'recoil'
 
 const PRIVATE_DEFAULT_SELECTED = `Nothing found matching the selected tags`
-const PRIVATE_DEFAULT_NOTHING_FOUND = `It seems like you do not have any words stored in our database. `
+const PRIVATE_DEFAULT_NOTHING_FOUND = `It seems like you do not have any words stored in our database`
 
 const WordCardsChunkNoWordsFound: FC = () => {
   const selectedSemester = useRecoilValue(selectedSemesterState)
