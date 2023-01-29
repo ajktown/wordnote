25d7aca9e1db460c795d6fc6d95abfb6cc4502c8
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-01-30 06:41:30 +0900

    feat: small

--- src/components/atom_word_card_favorite_icon/index.tsx
+++ src/components/atom_word_card_favorite_icon/index.tsx
@@ -4,6 +4,7 @@ import FavoriteWordIcon from '@mui/icons-material/FavoriteTwoTone'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { tempFavoriteWordIdsState, wordsFamily } from '@/recoil/words.state'
 import { usePutWord } from '@/hooks/words/use-put-word.hook'
+import { isFavoriteClickedState } from '@/recoil/favorites.state'
 
 interface Props {
   wordId: string
@@ -12,6 +13,7 @@ const WordCardFavoriteIcon: FC<Props> = ({ wordId }) => {
   const word = useRecoilValue(wordsFamily(wordId))
   const putWord = usePutWord(wordId)
   const [tempIds, setTempIds] = useRecoilState(tempFavoriteWordIdsState)
+  const isFavoriteClicked = useRecoilValue(isFavoriteClickedState)
 
   const handleClickFavoriteIcon = useCallback(async () => {
     if (word === null) return
@@ -19,8 +21,10 @@ const WordCardFavoriteIcon: FC<Props> = ({ wordId }) => {
     const modifyingTo = !word.isFavorite
     await putWord({ isFavorite: modifyingTo })
 
-    setTempIds([...tempIds, word.id])
-  }, [word, putWord, tempIds, setTempIds])
+    if (!isFavoriteClicked) return
+    if (modifyingTo) setTempIds([...tempIds].filter(id => id !== word.id))
+    else setTempIds([...tempIds, word.id])
+  }, [isFavoriteClicked, word, putWord, tempIds, setTempIds])
 
   if (word === null) return null
 
