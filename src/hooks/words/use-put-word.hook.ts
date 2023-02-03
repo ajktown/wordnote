b6b676ebcd51b718d16a324e04c023b6fb2550a4
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-02-04 08:02:38 +0900

    refactor: use put word

--- src/hooks/words/use-put-word.hook.ts
+++ src/hooks/words/use-put-word.hook.ts
@@ -1,17 +1,18 @@
 import { putWordByIdApi } from '@/api/words/put-word-by-id.api'
 import { WordDataModifiable } from '@/api/words/words.interface'
 import { wordsFamily } from '@/recoil/words.state'
-import { useCallback } from 'react'
 import { useRecoilCallback } from 'recoil'
 
 type UsePutWord = (modified: Partial<WordDataModifiable>) => Promise<void> // handlePutWord
 
 export const usePutWord = (wordId: string): UsePutWord => {
-  const setWord = useRecoilCallback(
+  const handlePutWord = useRecoilCallback(
     ({ snapshot, set }) =>
-      async (wordId: string, modified: Partial<WordDataModifiable>) => {
+      async (modified: Partial<WordDataModifiable>) => {
         const wordData = await snapshot.getPromise(wordsFamily(wordId))
         if (wordData === null) return
+
+        await putWordByIdApi(wordId, modified)
 
         set(wordsFamily(wordId), {
           ...wordData,
@@ -19,14 +20,6 @@ export const usePutWord = (wordId: string): UsePutWord => {
         })
       },
     [],
-  )
-
-  const handlePutWord: UsePutWord = useCallback(
-    async (modified: Partial<WordDataModifiable>) => {
-      await putWordByIdApi(wordId, modified)
-      setWord(wordId, modified)
-    },
-    [wordId, setWord],
   )
 
   return handlePutWord
