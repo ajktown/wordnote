2514c0922b9aae81810f40b2a82f19bae260c193
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-02-06 06:39:24 +0900

    feat: shows the customized tags

--- src/recoil/tags.state.ts
+++ src/recoil/tags.state.ts
@@ -1,11 +1,29 @@
-import { atomFamily } from 'recoil'
+import { atomFamily, selector } from 'recoil'
 import { RecoilKeySuffix } from './index.keys'
+import { semesterFilteredWordIds, wordsFamily } from './words.state'
 
 enum TagRecoilKey {
   SelectedTag = `SelectedTag`,
+  CustomizedTags = `CustomizedTags`,
 }
 
 export const selectedTagFamily = atomFamily<boolean, string>({
   key: TagRecoilKey.SelectedTag + RecoilKeySuffix.Family,
   default: false,
+})
+
+export const customizedTagsState = selector<string[]>({
+  key: TagRecoilKey.CustomizedTags + RecoilKeySuffix.Selector,
+  get: ({ get }) => {
+    const wordIds = get(semesterFilteredWordIds)
+    const tags = new Set<string>()
+
+    for (const wordId of wordIds) {
+      const word = get(wordsFamily(wordId))
+      if (!word) continue
+      word.tags.forEach(tag => tags.add(tag))
+    }
+
+    return Array.from(tags)
+  },
 })
