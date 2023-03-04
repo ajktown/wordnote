d7f8f5216eca3d6cad2da4be92639e3231cb7a02
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-03-05 08:32:09 +0900

    fix: file typo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import { axiosRequestSuccessLambda } from '@/lambdas/axios-request-error.lambda'
-import { axiosRequestErrorLambda } from '@/lambdas/axios-request-success.lambda'
+import { axiosRequestSuccessLambda } from '@/lambdas/axios-request-success.lambda'
+import { axiosRequestErrorLambda } from '@/lambdas/axios-request-error.lambda'
 import axios from 'axios'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
