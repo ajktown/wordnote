d1abe41827397b926054750e798908fde3373d6f
AJ Kim <jkim67cloud@gmail.com>
2023-03-27 19:37:59 +0900

    fix: typo

--- src/api/words/get-word-ids.api.ts
+++ src/api/words/get-word-ids.api.ts
@@ -5,7 +5,7 @@ import { GetWordParams } from './interfaces/index.search-params'
 export const getWordIdsApi = async (
   params?: Partial<GetWordParams>,
 ): Promise<CustomizedAxiosResponse<string[]>> => {
-  const url = `/v1/words`
+  const url = `/v1/words-ids`
   const res = await axios.get(url, { params })
   return [res.data, res]
 }
