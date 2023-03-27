c54a57df92262c89c855cb5b88e6c5ef77a64516
AJ Kim <jkim67cloud@gmail.com>
2023-03-28 05:28:04 +0900

    feat: downloads only when seen

--- src/components/molecule_word_card/index.tsx
+++ src/components/molecule_word_card/index.tsx
@@ -12,13 +12,15 @@ import {
 import StyledSuspense from '@/organisms/StyledSuspense'
 import WordCardEditingMode from './index.editing_mode'
 import TagButtonChunk from '../molecule_tag_button_chunk'
+import { useWordById } from '@/hooks/words/use-word-by-id.hook'
+import { useIsObserved } from '@/hooks/use-is-observed.hook'
 
 interface Props {
   wordId: string
   editingMode?: boolean
 }
 
-const WordCard: FC<Props> = ({ wordId, editingMode }) => {
+const WordCardBody: FC<Props> = ({ wordId, editingMode }) => {
   const word = useRecoilValue(wordsFamily(wordId))
   const setSelectedWordIdForDialog = useSetRecoilState(
     selectedWordIdForDialogState,
@@ -55,6 +57,17 @@ const WordCard: FC<Props> = ({ wordId, editingMode }) => {
         </CardActions>
       </Card>
     </StyledSuspense>
+  )
+}
+
+const WordCard: FC<Props> = (props) => {
+  const handleGetWord = useWordById(props.wordId)
+  const ref = useIsObserved(handleGetWord)
+
+  return (
+    <span ref={ref} style={{ width: `100%` }}>
+      <WordCardBody {...props} />
+    </span>
   )
 }
 
