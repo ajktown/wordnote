69e05bf9f9681e56375b0e35394975be9e907e15
AJ Kim <jkim67cloud@gmail.com>
2023-04-27 08:38:07 +0900

    feat: continue with google with oauth constant

--- src/components/atom_continue_with/index.google.tsx
+++ src/components/atom_continue_with/index.google.tsx
@@ -1,7 +1,6 @@
 import { FC, useCallback } from 'react'
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
-
-const PRIVATE_GOOGLE_CLIENT_ID = `1029911886121-ir1hcpgk3hpanlaatkf61g6f2t8aanqo.apps.googleusercontent.com`
+import { OauthConst } from '@/constants/oauth.constant'
 
 const ContinueWithGoogle: FC = () => {
   const onSuccess = useCallback(() => {
@@ -13,8 +12,12 @@ const ContinueWithGoogle: FC = () => {
   }, [])
 
   return (
-    <GoogleOAuthProvider clientId={PRIVATE_GOOGLE_CLIENT_ID}>
-      <GoogleLogin onSuccess={onSuccess} onError={onError} useOneTap />
+    <GoogleOAuthProvider clientId={OauthConst.GoogleClientId}>
+      <GoogleLogin
+        onSuccess={onSuccess}
+        onError={onError}
+        text="continue_with"
+      />
     </GoogleOAuthProvider>
   )
 }
---
+++ src/constants/oauth.constant.ts
@@ -0,0 +1,3 @@
+export enum OauthConst {
+  GoogleClientId = `1029911886121-ir1hcpgk3hpanlaatkf61g6f2t8aanqo.apps.googleusercontent.com`,
+}
