ff89bde016994882778830445395b247ee939149
AJ Kim <jkim67cloud@gmail.com>
2023-05-15 15:52:09 +0900

    feat: on sign out applied for api error

--- src/hooks/use-api-error.hook.ts
+++ src/hooks/use-api-error.hook.ts
@@ -1,18 +1,25 @@
+import { CustomizedApiError } from '@/errors'
 import { isApiConnectFailed } from '@/recoil/apis/error-api-connection-fail.state'
 import { useRecoilCallback } from 'recoil'
+import { useOnClickSignOutApp } from './\bapp/use-on-click-sign-out-app'
 
 type HandleApiError = (err: unknown) => any
 type UseApiErrorHook = HandleApiError
 
 export const useApiErrorHook = (): UseApiErrorHook => {
+  const onSignOut = useOnClickSignOutApp()
+
   const handleApiError: HandleApiError = useRecoilCallback(
     ({ set }) =>
       async (err: unknown) => {
-        console.log(err) // TODO: Actually use it
-
         set(isApiConnectFailed, true)
+
+        const error = CustomizedApiError.fromUnknown(err)
+        if (error.props.statusCode === 401) {
+          await onSignOut()
+        }
       },
-    [],
+    [onSignOut],
   )
 
   return handleApiError
