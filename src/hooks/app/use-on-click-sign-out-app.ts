851d9cd9acb06c1abfc457eaabeb3ed2dd859ec0
AJ Kim <jkim67cloud@gmail.com>
2023-05-15 13:41:12 +0900

    feat: sign out applied

--- src/hooks/app/use-on-click-sign-out-app.ts
+++ src/hooks/app/use-on-click-sign-out-app.ts
@@ -2,6 +2,7 @@ import { useRecoilCallback } from 'recoil'
 import { useRouter } from 'next/router'
 import { PageConst } from '@/constants/pages.constant'
 import { wordIdsState } from '@/recoil/words/words.state'
+import { postSignOut } from '@/api/auth/post-sign-out.api'
 
 export const useOnClickSignOutApp = () => {
   const router = useRouter()
@@ -9,6 +10,7 @@ export const useOnClickSignOutApp = () => {
   const onClickSignOut = useRecoilCallback(
     ({ reset }) =>
       async () => {
+        await postSignOut()
         reset(wordIdsState)
         router.push(PageConst.Welcome)
       },
