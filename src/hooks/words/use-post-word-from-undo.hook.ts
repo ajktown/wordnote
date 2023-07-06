caa2ad5891231cad35afba938da23ca6d98039e5
AJ Kim <jkim67cloud@gmail.com>
2023-07-07 06:35:56 +0900

    feat: set semesters from returned post word

--- src/hooks/words/use-post-word-from-undo.hook.ts
+++ src/hooks/words/use-post-word-from-undo.hook.ts
@@ -2,6 +2,7 @@ import { postWordApi } from '@/api/words/post-word.api'
 import { wordsFamily } from '@/recoil/words/words.state'
 import { useRecoilCallback } from 'recoil'
 import { wordIdsState } from '@/recoil/words/words.state'
+import { semestersState } from '@/recoil/words/semesters.state'
 
 type UsePostWordFromUndo = () => Promise<void> // handlePostWordFromUndo
 
@@ -14,14 +15,15 @@ export const usePostWordFromUndo = (
         const word = await snapshot.getPromise(wordsFamily(undoingWordId))
         if (!word) return // failed to re-post word
 
-        const [recreatedWord] = await postWordApi(word)
+        const [{ postedWord, semesters }] = await postWordApi(word)
         const wordIds = (await snapshot.getPromise(wordIdsState)).map((id) => {
-          if (id === undoingWordId) return recreatedWord.id
+          if (id === undoingWordId) return postedWord.id
           return id
         })
 
         set(wordIdsState, wordIds)
-        set(wordsFamily(recreatedWord.id), recreatedWord)
+        set(wordsFamily(postedWord.id), postedWord)
+        set(semestersState, semesters.semesters)
       },
     [undoingWordId],
   )
