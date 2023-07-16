f2e429348e595b729a8567cc59da9f15ed743d6f
AJ Kim <jkim67cloud@gmail.com>
2023-07-16 17:04:03 +0900

    feat: deployment works

--- src/lambdas/axios-request-success.lambda.ts
+++ src/lambdas/axios-request-success.lambda.ts
@@ -1,10 +1,14 @@
 import { InternalAxiosRequestConfig } from 'axios'
 
+const PRIVATE_DEFAULT_API_URL = `http://localhost:8000`
 export const axiosRequestSuccessLambda = (
   config: InternalAxiosRequestConfig<any>,
 ) => {
   // Do something before request is sent
-  config.url = `http://localhost:8000/api` + config.url
+  const api_url: string =
+    process.env.NEXT_PUBLIC_API_URL ?? PRIVATE_DEFAULT_API_URL
+
+  config.url = `${api_url}/api` + config.url
   console.log(`Requesting to URL: ` + config.url)
   console.log(`Params: ` + JSON.stringify(config.params))
 
