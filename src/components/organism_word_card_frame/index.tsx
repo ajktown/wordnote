4e35d8008ef41c7e25d339584dcd687c7b48d394
Akane Oba <aka1105maron@gmail.com>
2023-07-27 01:50:46 +0900

    create : surf-icon

--- src/components/organism_word_card_frame/index.tsx
+++ src/components/organism_word_card_frame/index.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import { Stack, Box } from '@mui/material'
 import NewWordBox from '../molecule_new_word_box'
 import WordCardsFrameRefreshButton from '../atom_word_cards_frame_refresh_button'
+import WordCardsSurfingButton from '../atom_card_frame_surfing_botton'
 import WordCardsChunk from '../organism_word_card_chunk'
 import { WordCardFrameStyle } from './index.style'
 import WordCardDialog from '../molecule_word_card/index.dialog'
@@ -13,8 +14,8 @@ const WordCardFrame: FC = () => {
     <Stack width="100%" alignItems="center">
       <Stack {...WordCardFrameStyle}>
         {/* Header */}
-        <Stack direction="row" spacing={0.5}>
-          <Box flexGrow={1} />
+        <Stack direction="row" spacing={0.5} alignItems="center">
+          <WordCardsSurfingButton /> {/* surfアイコンを追加 */}
           <WordCardsFrameRefreshButton />
         </Stack>
         <Stack alignItems={`center`} spacing={0.35}>
