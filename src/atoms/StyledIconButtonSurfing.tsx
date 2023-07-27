63d380e46301ca996e98dc8e87c058b8ad02da6f
Akane Oba <aka1105maron@gmail.com>
2023-07-27 22:18:06 +0900

    fix : file name

---
+++ src/atoms/StyledIconButtonSurfing.tsx
@@ -0,0 +1,28 @@
+import { FC, useCallback } from 'react'
+import CloseIcon from '@mui/icons-material/Close'
+import StyledIconButtonAtom from '@/atoms/StyledIconButton'
+import { GlobalMuiSize } from '@/global.interface'
+import SurfingIcon from '@mui/icons-material/Surfing'
+
+interface Props {
+    onClick?: any
+    hoverMessage?: string
+    buttonColor?: string
+    size?: GlobalMuiSize
+}
+
+const StyledIconButtonSurfing: FC<Props> = ({ onClick, ...props }) => {
+  const handleClickCallback = useCallback(() => {
+    onClick && onClick()
+  }, [onClick])
+
+  return (
+    <StyledIconButtonAtom
+      size={props.size}
+      onClick={handleClickCallback}
+      jsxElementButton={<CloseIcon fontSize={props.size} />}
+    />
+  )
+}
+
+export default SurfingIcon
