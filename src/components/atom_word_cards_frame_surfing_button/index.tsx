c23896fe77cc99f0aab427eef3741b1fd68c7586
Akane Oba <aka1105maron@gmail.com>
2023-07-30 23:11:59 +0900

    fix: get latest semester

--- src/components/atom_word_cards_frame_surfing_button/index.tsx
+++ src/components/atom_word_cards_frame_surfing_button/index.tsx
@@ -1,14 +1,22 @@
-import { FC } from 'react'
 import StyledIconButtonAtom from '@/atoms/StyledIconButton'
 import SurfingIcon from '@mui/icons-material/Surfing'
+import { FC, useCallback } from 'react'
+import { useSemesters } from '@/hooks/semesters/use-semesters.hook'
+import { useWords } from '@/hooks/words/use-words.hook'
 
 const WordCardsFrameSurfingButton: FC = () => {
-  const onClick = () => {
-    console.log(`Hello World!`)
-  }
+  const [, getWords] = useWords()
+  const getSemesters = useSemesters()
+
+  const onClickRefresh = useCallback(async () => {
+    const semesters = await getSemesters()
+    if (!semesters.latestSemesterCode) return
+
+    await getWords({ semester: semesters.latestSemesterCode })
+  }, [getWords, getSemesters])
   return (
     <StyledIconButtonAtom
-      onClick={onClick}
+      onClick={onClickRefresh}
       jsxElementButton={<SurfingIcon fontSize="small" />}
     />
   )
