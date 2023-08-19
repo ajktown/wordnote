b2fdb490e3d5c2bf4111f8fdfee37003a220e287
AJ Kim <jkim67cloud@gmail.com>
2023-08-19 15:01:07 +0900

    feat: only trimmed data

--- src/components/atom_word_card_parts/index.example.tsx
+++ src/components/atom_word_card_parts/index.example.tsx
@@ -1,20 +1,26 @@
 import { FC } from 'react'
 import { WordData } from '@/api/words/interfaces'
 import Link from 'next/link'
+import { Typography } from '@mui/material'
 
 interface Props {
   word: WordData
 }
 const WordCardExamplePart: FC<Props> = ({ word }) => {
-  if (word.example.trim() === `` && word.exampleLink)
+  const trimmedExample = word.example.trim()
+  const trimmedExampleLink = word.exampleLink.trim()
+
+  if (trimmedExample === `` && trimmedExampleLink)
     return (
-      <Link href={word.exampleLink} target="_blank">
+      <Link href={trimmedExampleLink} target="_blank">
         Sample Example
       </Link>
     )
 
+  if (trimmedExampleLink === ``) return <Typography>{trimmedExampleLink}</Typography>
+
   return (
-    <Link href={word.exampleLink} target="_blank">
+    <Link href={trimmedExampleLink} target="_blank">
       {word.example}
     </Link>
   )
