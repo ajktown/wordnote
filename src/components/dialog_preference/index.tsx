46e4ff688f3fe2ce4064bd764e51b931d198e9c3
AJ Kim <jkim67cloud@gmail.com>
2023-08-26 10:01:45 +0900

    feat: on click example

--- src/components/dialog_preference/index.tsx
+++ src/components/dialog_preference/index.tsx
@@ -1,4 +1,5 @@
 import StyledTextButtonAtom from '@/atoms/StyledTextButton'
+import { GlobalLanguageCode } from '@/global.interface'
 import StyledDialog from '@/organisms/StyledDialog'
 import {
   PreferenceState,
@@ -13,14 +14,45 @@ import {
   FormControlLabel,
   FormGroup,
 } from '@mui/material'
-import { FC } from 'react'
-import { useRecoilValue, useResetRecoilState } from 'recoil'
+import { FC, SyntheticEvent } from 'react'
+import { useRecoilCallback, useRecoilValue, useResetRecoilState } from 'recoil'
+
+const privateSupportedLanguages: { [key: string]: GlobalLanguageCode } = {
+  English: `en`,
+  Chinese: `zh`,
+  Japanese: `ja`,
+  Korean: `ko`,
+}
 
 const PreferenceDialog: FC = () => {
   const isPreferenceDialogOpened = useRecoilValue(isPreferenceDialogOpenedState)
-  const Preference = useRecoilValue(PreferenceState)
+  const preference = useRecoilValue(PreferenceState)
   const resetPreferenceDialogOpenedState = useResetRecoilState(
     isPreferenceDialogOpenedState,
+  )
+
+  const onChange = useRecoilCallback(
+    ({ set, snapshot }) =>
+      async (e: SyntheticEvent<Element, Event>, checked: boolean) => {
+        const preference = await snapshot.getPromise(PreferenceState)
+        if (!preference) return
+
+        const element = e.target as HTMLInputElement
+        const languageCode = element.value as GlobalLanguageCode
+        const nativeLanguagesSet = new Set([
+          ...preference.nativeLanguages,
+          languageCode,
+        ])
+
+        if (!checked) nativeLanguagesSet.delete(languageCode)
+        const nativeLanguages: GlobalLanguageCode[] =
+          Array.from(nativeLanguagesSet)
+
+        set(PreferenceState, {
+          ...preference,
+          nativeLanguages,
+        })
+      },
   )
 
   if (!isPreferenceDialogOpened) return null
@@ -37,23 +69,31 @@ const PreferenceDialog: FC = () => {
         </DialogContentText>
         <FormGroup>
           <FormControlLabel
-            checked={Preference?.nativeLanguages.includes(`en`)}
+            checked={preference?.nativeLanguages.includes(`en`)}
             control={<Checkbox />}
+            onChange={onChange}
+            value={privateSupportedLanguages.English}
             label="English"
           />
           <FormControlLabel
-            checked={Preference?.nativeLanguages.includes(`zh`)}
+            checked={preference?.nativeLanguages.includes(`zh`)}
             control={<Checkbox />}
+            onChange={onChange}
+            value={privateSupportedLanguages.Chinese}
             label="Chinese"
           />
           <FormControlLabel
-            checked={Preference?.nativeLanguages.includes(`ja`)}
+            checked={preference?.nativeLanguages.includes(`ja`)}
             control={<Checkbox />}
+            onChange={onChange}
+            value={privateSupportedLanguages.Japanese}
             label="Japanese"
           />
           <FormControlLabel
-            checked={Preference?.nativeLanguages.includes(`ko`)}
+            checked={preference?.nativeLanguages.includes(`ko`)}
             control={<Checkbox />}
+            onChange={onChange}
+            value={privateSupportedLanguages.Korean}
             label="Korean"
           />
         </FormGroup>
