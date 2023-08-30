ffdfe0e9acbb7338135a48e4cf3db7d2b2712c68
Akane Oba <aka1105maron@gmail.com>
2023-08-30 15:37:31 +0900

    feat: ko-ko,ja-ko,ko-ja dict null conditions

--- src/components/molecule_dict_link_button_chunk/ko-naver.dict-link-button.tsx
+++ src/components/molecule_dict_link_button_chunk/ko-naver.dict-link-button.tsx
@@ -3,6 +3,7 @@ import { useRecoilValue } from 'recoil'
 import { wordsFamily } from '@/recoil/words/words.state'
 import { useOpenNewTab } from '@/hooks/use-open-new-tab'
 import StyledImageButtonAtom from '@/atoms/StyledImageButton'
+import { preferenceState } from '@/recoil/preferences/preference.state'
 
 const KO_NAVER_DICTIONARY_PREFIX = `https://ko.dict.naver.com/#/search?range=all&query=`
 
@@ -10,11 +11,13 @@ interface Props {
   wordId: string
 }
 const KoNaverDictionaryLinkButton: FC<Props> = ({ wordId }) => {
+  const preference = useRecoilValue(preferenceState)
   const word = useRecoilValue(wordsFamily(wordId))
   const link = KO_NAVER_DICTIONARY_PREFIX + word?.term
   const onClick = useOpenNewTab(link)
 
   if (!word || word.languageCode !== `ko` || !word.term) return null
+  if (!preference?.nativeLanguages.includes(`ko`)) return null
 
   return (
     <StyledImageButtonAtom
