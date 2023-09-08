cba057f35911f62cb8fa77a2ce5c7dcf4bdeb50b
Akane Oba <aka1105maron@gmail.com>
2023-09-08 19:45:28 +0900

    fix: delete onChange and value

--- src/components/atom_preference_language_checkbox/index.tsx
+++ src/components/atom_preference_language_checkbox/index.tsx
@@ -1,8 +1,8 @@
 import { GlobalLanguageCode } from '@/global.interface'
 import { preferenceState } from '@/recoil/preferences/preference.state'
 import { Checkbox, FormControlLabel } from '@mui/material'
-import { FC, SyntheticEvent } from 'react'
-import { useRecoilCallback, useRecoilValue } from 'recoil'
+import { FC } from 'react'
+import { useRecoilValue } from 'recoil'
 
 interface Props {
   languageCode: GlobalLanguageCode
@@ -10,35 +10,12 @@ interface Props {
 
 const PreferenceLanguageCheckbox: FC<Props> = ({ languageCode }) => {
   const preference = useRecoilValue(preferenceState)
-  const onChange = useRecoilCallback(
-    ({ set, snapshot }) =>
-      async (e: SyntheticEvent<Element, Event>, checked: boolean) => {
-        const preference = await snapshot.getPromise(preferenceState)
-        if (!preference) return
-
-        const nativeLanguagesSet = new Set([
-          ...preference.nativeLanguages,
-          languageCode,
-        ])
-
-        if (!checked) nativeLanguagesSet.delete(languageCode)
-        const nativeLanguages: GlobalLanguageCode[] =
-          Array.from(nativeLanguagesSet)
-
-        set(preferenceState, {
-          ...preference,
-          nativeLanguages,
-        })
-      },
-  )
 
   return (
     <FormControlLabel
       checked={preference?.nativeLanguages.includes(languageCode)}
       control={<Checkbox />}
       label={languageCode}
-      onChange={onChange}
-      value={languageCode}
     />
   )
 }
