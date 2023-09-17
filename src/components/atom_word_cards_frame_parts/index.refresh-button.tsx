94421a5c9173f73468e4e504334a56071585935b
Akane Oba <aka1105maron@gmail.com>
2023-09-17 16:55:10 +0900

    feat: setPreferenceState

--- src/components/atom_word_cards_frame_parts/index.refresh-button.tsx
+++ src/components/atom_word_cards_frame_parts/index.refresh-button.tsx
@@ -2,17 +2,24 @@ import { FC, useCallback } from 'react'
 import StyledCloudRefresher from '@/atoms/StyledCloudRefresher'
 import { useSemesters } from '@/hooks/semesters/use-semesters.hook'
 import { useWords } from '@/hooks/words/use-words.hook'
+import { useSetRecoilState } from 'recoil'
+import { preferenceState } from '@/recoil/preferences/preference.state'
+import { getPreferenceApi } from '@/api/preferences/get-preferences.api'
 
 const WordCardsFrameRefreshButtonPart: FC = () => {
   const [, getWords] = useWords()
   const getSemesters = useSemesters()
+  const setPreferenceState = useSetRecoilState(preferenceState)
 
   const onClickRefresh = useCallback(async () => {
     const semesters = await getSemesters()
     if (!semesters.latestSemesterCode) return
 
     await getWords({ semester: semesters.latestSemesterCode })
-  }, [getWords, getSemesters])
+    
+    const [data] = await getPreferenceApi()
+    setPreferenceState(data)
+  }, [getWords, getSemesters,setPreferenceState])
 
   return <StyledCloudRefresher onClick={onClickRefresh} runOnClickOnce />
 }
