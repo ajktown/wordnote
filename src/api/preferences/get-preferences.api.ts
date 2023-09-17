fd26f66574c7c1a7880c613e669062944d97c7a9
Akane Oba <aka1105maron@gmail.com>
2023-09-17 16:53:29 +0900

    fix: url of preference(s)

--- src/api/preferences/get-preferences.api.ts
+++ src/api/preferences/get-preferences.api.ts
@@ -2,10 +2,12 @@ import axios from 'axios'
 import { CustomizedAxiosResponse } from '../index.interface'
 import { IPreference } from './index.interface'
 
-export const getPreferencesApi = async (): Promise<
+export const getPreferenceApi = async (): Promise<
   CustomizedAxiosResponse<IPreference>
 > => {
-  const url = `/v1/preferences`
+  const url = `/v1/preference`
   const res = await axios.get(url)
+  console.log({getPreferenceApi})
+
   return [res.data, res]
 }
