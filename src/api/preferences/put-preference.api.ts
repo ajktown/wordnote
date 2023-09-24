e4fbfbe933c9dec27caa4004a494fca8a8f75f53
Akane Oba <aka1105maron@gmail.com>
2023-09-24 14:46:51 +0900

    yarn: inspect

--- src/api/preferences/put-preference.api.ts
+++ src/api/preferences/put-preference.api.ts
@@ -4,9 +4,7 @@ import { IPreference, PreferenceModifiable } from './index.interface'
 
 export const putPreferenceApi = async (
   modifying: Partial<PreferenceModifiable>,
-): Promise<
-  CustomizedAxiosResponse<IPreference>
-> => {
+): Promise<CustomizedAxiosResponse<IPreference>> => {
   const url = `/v1/preference`
   const res = await axios.put(url, modifying)
   return [res.data, res]
