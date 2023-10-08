4328495e296f2d8c70d4016155dddfd277b0f281
AJ Kim <jkim67cloud@gmail.com>
2023-10-08 12:10:09 +0900

    fix: onChange

--- src/components/atom_preference_language_checkbox/index.tsx
+++ src/components/atom_preference_language_checkbox/index.tsx
@@ -1,31 +1,25 @@
-import { putPreferenceApi } from '@/api/preferences/put-preference.api'
 import { getLanguageFullName } from '@/global.constants'
 import { GlobalLanguageCode } from '@/global.interface'
 import { usePutPreference } from '@/hooks/preference/use-put-preference.hook'
 import { preferenceState } from '@/recoil/preferences/preference.state'
 import { Checkbox, FormControlLabel } from '@mui/material'
 import { FC } from 'react'
-import { useRecoilCallback, useRecoilValue } from 'recoil'
+import { useRecoilValue } from 'recoil'
 
 interface Props {
   languageCode: GlobalLanguageCode
 }
 
 const PreferenceLanguageCheckbox: FC<Props> = ({ languageCode }) => {
-  const putPreference = usePutPreference(languageCode)
+  const onPutPreference = usePutPreference(languageCode)
   const preference = useRecoilValue(preferenceState)
-  const onChange = useRecoilCallback(
-    ()=>{
-      putPreference({})
-    }
-  )
 
   return (
     <FormControlLabel
       checked={preference?.nativeLanguages.includes(languageCode)}
       control={<Checkbox />}
       label={getLanguageFullName(languageCode)}
-      onChange={onChange}
+      onChange={onPutPreference}
     />
   )
 }
--- src/hooks/preference/use-put-preference.hook.ts
+++ src/hooks/preference/use-put-preference.hook.ts
@@ -3,29 +3,29 @@ import { preferenceState } from '@/recoil/preferences/preference.state'
 import { putPreferenceApi } from '@/api/preferences/put-preference.api'
 import { GlobalLanguageCode } from '@/global.interface'
 
-export const usePutPreference = (languageCode:GlobalLanguageCode) => {
-  const onClick = useRecoilCallback(
+export const usePutPreference = (languageCode: GlobalLanguageCode) => {
+  const onPutPreference = useRecoilCallback(
     ({ set, snapshot }) =>
-    async (_, checked: boolean) => {
-      try {
-        const preference = await snapshot.getPromise(preferenceState)
-        if (!preference) return
+      async (_, checked: boolean) => {
+        try {
+          const preference = await snapshot.getPromise(preferenceState)
+          if (!preference) return
 
-        const nativeLanguagesSet = new Set([
-          ...preference.nativeLanguages,
-          languageCode,
-        ])
+          const nativeLanguagesSet = new Set([
+            ...preference.nativeLanguages,
+            languageCode,
+          ])
 
-        if (!checked) nativeLanguagesSet.delete(languageCode)
-        const nativeLanguages: GlobalLanguageCode[] =
-          Array.from(nativeLanguagesSet)
+          if (!checked) nativeLanguagesSet.delete(languageCode)
+          const nativeLanguages: GlobalLanguageCode[] =
+            Array.from(nativeLanguagesSet)
 
-        const [data] = await putPreferenceApi({ nativeLanguages })
-        set(preferenceState, data)
-      } catch {}
-    },
-  [languageCode],
+          const [data] = await putPreferenceApi({ nativeLanguages })
+          set(preferenceState, data)
+        } catch {}
+      },
+    [languageCode],
   )
 
-  return onClick
+  return onPutPreference
 }
