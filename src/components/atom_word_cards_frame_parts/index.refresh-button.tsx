b355fe857cc80daddb199d829f06a29edf9914cd
Akane Oba <aka1105maron@gmail.com>
2023-11-08 00:58:46 +0900

    fix: refresh bug

--- src/components/atom_word_cards_frame_parts/index.refresh-button.tsx
+++ src/components/atom_word_cards_frame_parts/index.refresh-button.tsx
@@ -3,20 +3,23 @@ import StyledCloudRefresher from '@/atoms/StyledCloudRefresher'
 import { useSemesters } from '@/hooks/semesters/use-semesters.hook'
 import { useWords } from '@/hooks/words/use-words.hook'
 import { usePreference } from '@/hooks/preference/use-preference.hook'
+import { useRecoilValue } from 'recoil'
+import { selectedSemesterSelector } from '@/recoil/words/words.selectors'
 
 const WordCardsFrameRefreshButtonPart: FC = () => {
   const [, getWords] = useWords()
   const getSemesters = useSemesters()
   const getPreference = usePreference()
+  const selectedSemester = useRecoilValue(selectedSemesterSelector)
 
   const onClickRefresh = useCallback(async () => {
     const semesters = await getSemesters()
     if (!semesters.latestSemesterCode) return
 
-    await getWords({ semester: semesters.latestSemesterCode })
+    await getWords({ semester: selectedSemester })
 
     await getPreference()
-  }, [getWords, getSemesters, getPreference])
+  }, [getWords, getSemesters, getPreference, selectedSemester])
 
   return <StyledCloudRefresher onClick={onClickRefresh} runOnClickOnce />
 }
