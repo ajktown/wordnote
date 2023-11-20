ce1243bc48f6b7bf0c24c1ef69712df5a9e5e9d8
AJ Kim <jkim67cloud@gmail.com>
2023-11-21 08:58:13 +0900

    refactor: with hook

--- src/components/atom_word_cards_frame_parts/index.refresh-button.tsx
+++ src/components/atom_word_cards_frame_parts/index.refresh-button.tsx
@@ -1,32 +1,16 @@
 import { FC, useCallback } from 'react'
 import StyledCloudRefresher from '@/atoms/StyledCloudRefresher'
-import { useSemesters } from '@/hooks/semesters/use-semesters.hook'
-import { useWords } from '@/hooks/words/use-words.hook'
 import { usePreference } from '@/hooks/preference/use-preference.hook'
-import { useRecoilValue } from 'recoil'
-import { selectedSemesterSelector } from '@/recoil/words/words.selectors'
+import { useWordsWithSemesters } from '@/hooks/words/use-words-with-semesters.hook'
 
 const WordCardsFrameRefreshButtonPart: FC = () => {
-  const [, getWords] = useWords()
-  const getSemesters = useSemesters()
-  const getPreference = usePreference()
-  const selectedSemester = useRecoilValue(selectedSemesterSelector)
-
-  /**
-   * Sync words with the API Server. If selectedSemester is undefined,
-   * the API will understand and return the latest semester.
-   */
-  const onGetWordsSync = useCallback(async () => {
-    const semesters = await getSemesters()
-    if (!semesters.latestSemesterCode) return // User has never created word even once.
-
-    await getWords({ semester: selectedSemester })
-  }, [selectedSemester, getSemesters, getWords])
+  const onGetPreference = usePreference()
+  const onGetWordsWithSemesters = useWordsWithSemesters()
 
   const onClickRefresh = useCallback(async () => {
     // run all together
-    await Promise.all([onGetWordsSync(), getPreference()])
-  }, [onGetWordsSync, getPreference])
+    await Promise.all([onGetWordsWithSemesters(), onGetPreference()])
+  }, [onGetWordsWithSemesters, onGetPreference])
 
   return <StyledCloudRefresher onClick={onClickRefresh} runOnClickOnce />
 }
