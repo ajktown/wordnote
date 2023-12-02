76716f68bc2a7fa90474c1ba7d47b3f8e0aeeb95
AJ Kim <jkim67cloud@gmail.com>
2023-12-02 13:16:39 +0900

    feat: FirstTimeUserConfetti with only new user signed in

--- src/components/organism_first_user_confetti/index.tsx
+++ src/components/organism_first_user_confetti/index.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react'
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
+import { useRecoilValue } from 'recoil'
+import { authPrepState } from '@/recoil/app/app.state'
 
 const FirstTimeUserConfetti: FC = () => {
+  const authPrep = useRecoilValue(authPrepState)
   const { width, height } = useWindowSize()
 
+  if (!authPrep?.signedInUserInfo?.isFirstTimeUser) return null
   return <Confetti width={width} height={height} />
 }
 
