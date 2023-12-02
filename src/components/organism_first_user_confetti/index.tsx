1f7fb053e584fd3218313c036d84c0de7371ff2b
AJ Kim <jkim67cloud@gmail.com>
2023-12-02 16:38:29 +0900

    feat: run only once FirstTimeUserConfetti

--- src/components/organism_first_user_confetti/index.tsx
+++ src/components/organism_first_user_confetti/index.tsx
@@ -5,11 +5,11 @@ import { useRecoilValue } from 'recoil'
 import { authPrepState } from '@/recoil/app/app.state'
 
 const FirstTimeUserConfetti: FC = () => {
-  const authPrep = useRecoilValue(authPrepState)
   const { width, height } = useWindowSize()
+  const authPrep = useRecoilValue(authPrepState)
 
   if (!authPrep?.signedInUserInfo?.isFirstTimeUser) return null
-  return <Confetti width={width} height={height} />
+  return <Confetti width={width} height={height} recycle={false} />
 }
 
 export default FirstTimeUserConfetti
