2701d9f006eb1af8cac6324529ec37e3296bb3f1
AJ Kim <jkim67cloud@gmail.com>
2023-12-02 13:16:44 +0900

    feat: apply FirstTimeUserConfetti

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react'
 import Appbar from '../components/organism_appbar'
 import HomeLayout from '../layouts'
+import FirstTimeUserConfetti from '@/components/organism_first_user_confetti'
 
 const HomePage: FC = () => {
   return (
     <Appbar>
+      <FirstTimeUserConfetti />
       <HomeLayout />
     </Appbar>
   )
