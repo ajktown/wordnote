5eef463c825b108e30734f31aaac432ce3dc94cc
AJ Kim <jkim67cloud@gmail.com>
2023-12-04 07:34:17 +0900

    feat: x button for close message

--- src/components/molecule_first_time_user_welcome_message/index.tsx
+++ src/components/molecule_first_time_user_welcome_message/index.tsx
@@ -1,15 +1,18 @@
 import { isFirstTimeUserSelector } from '@/recoil/app/app.selectors'
 import { Alert, Typography } from '@mui/material'
-import { FC } from 'react'
+import { FC, useCallback, useState } from 'react'
 import { useRecoilValue } from 'recoil'
 
 const FirstTimeUserWelcomeMessage: FC = () => {
+  const [isClosed, setClosed] = useState<boolean>(false)
   const isFirstTimeUser = useRecoilValue(isFirstTimeUserSelector)
 
-  if (!isFirstTimeUser) return null
+  const onClose = useCallback(() => setClosed(true), [])
+
+  if (!isFirstTimeUser || isClosed) return null
 
   return (
-    <Alert severity="info">
+    <Alert severity="info" onClose={onClose}>
       <Typography variant="caption">
         {`🎉 Congratulations, you've just joined Wordnote - AJK Town!`}
       </Typography>
