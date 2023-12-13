087cff2845872e4306a53e25e636d2ec6e80a5aa
AJ Kim <jkim67cloud@gmail.com>
2023-12-14 05:58:04 +0900

    feat: does not require sign in

--- src/hooks/app/use-is-app-booted.hook.ts
+++ src/hooks/app/use-is-app-booted.hook.ts
@@ -2,7 +2,7 @@ import { isAppBootedSelector } from '@/recoil/app/app.state'
 import { useEffect } from 'react'
 import { useRecoilCallback, useRecoilValue } from 'recoil'
 import { useRouter } from 'next/router'
-import { DEFAULT_MAIN_APP_PAGE } from '@/constants/pages.constant'
+import { DEFAULT_MAIN_APP_PAGE, PageConst } from '@/constants/pages.constant'
 import { useOnSignOutApp } from './use-on-sign-out-app.hook'
 import { useAuthPrep } from '../auth/use-auth-prep.hook'
 
@@ -16,7 +16,11 @@ export const useIsAppBooted = (): boolean => {
     () => async () => {
       try {
         const authPrep = await onGetAuthPrep()
-        if (!authPrep || !authPrep.isSignedIn) throw new Error(`Not Signed In`)
+
+        // The page share does not require sign in. But it should get the auth prep data just in case.
+        if (router.pathname === PageConst.Share) return
+        if (!authPrep?.isSignedIn) throw new Error(`Not Signed In`)
+
         router.push(DEFAULT_MAIN_APP_PAGE)
       } catch {
         await handleSignOutApp()
