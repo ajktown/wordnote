82c924cbfc8d76410764be5362699d0e5b362594
AJ Kim <jkim67cloud@gmail.com>
2024-01-20 11:40:47 +0900

    feat: onHandleExpire

--- src/atoms/StyledCountdownTimer.tsx
+++ src/atoms/StyledCountdownTimer.tsx
@@ -3,18 +3,23 @@ import { Typography } from '@mui/material'
 
 interface Props {
   targetTime: number | null // JS valueOf() time.
+  onHandleExpire?: () => void // a function that will be called when the timer expires as 0
 }
 /**
  * StyledCountdownTimer is a component that shows the time left until the target time.
  */
 // TODO: make the design better.
-const StyledCountdownTimer: FC<Props> = ({ targetTime }) => {
+const StyledCountdownTimer: FC<Props> = ({ targetTime, onHandleExpire }) => {
   const calculateTimeLeft = useCallback(() => {
     if (targetTime === null) return 0 // expired
 
     const difference = +new Date(targetTime) - +new Date()
-    return Math.max(Math.floor(difference / 1000), 0)
-  }, [targetTime])
+    const leftOverSeconds = Math.max(Math.floor(difference / 1000), 0)
+
+    // if leftOverSeconds is 0, then run onHandleExpire
+    if (leftOverSeconds === 0 && onHandleExpire) onHandleExpire()
+    return leftOverSeconds
+  }, [targetTime, onHandleExpire])
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
 
   useEffect(() => {
