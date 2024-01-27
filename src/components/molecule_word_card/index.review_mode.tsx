0e1854f9358930c9c1a5c009e77c9a9e2edbc3af
AJ Kim <jkim67cloud@gmail.com>
2024-01-27 13:09:20 +0900

    feat: more action buttons

--- src/components/molecule_word_card/index.review_mode.tsx
+++ src/components/molecule_word_card/index.review_mode.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useState } from 'react'
+import { FC, useState } from 'react'
 import { Card, CardActions, CardContent, Typography } from '@mui/material'
 import WordCardFavoriteIcon from '../atom_word_card_favorite_icon'
 import StyledSuspense from '@/organisms/StyledSuspense'
@@ -7,9 +7,11 @@ import TagButtonChunk from '../molecule_tag_button_chunk'
 import WordCardExamplePart from '../atom_word_card_parts/index.example'
 import StyledVisibilityAtom from '@/atoms/StyledVisibility'
 import { selectedWordIdForDialogState } from '@/recoil/words/words.state'
-import { useSetRecoilState } from 'recoil'
-import StyledIconButtonAtom from '@/atoms/StyledIconButton'
-import EditIcon from '@mui/icons-material/Edit'
+import { useRecoilCallback } from 'recoil'
+import WordCardArchiveButtonPart from '../atom_word_card_parts/index.archive-button'
+import WordCardUnarchiveButtonPart from '../atom_word_card_parts/index.unarchive-button'
+import WordCardShareButtonPart from '../atom_word_card_parts/index.share-button'
+import DictLinkButtonChunk from '../molecule_dict_link_button_chunk'
 interface Props {
   word: WordData
 }
@@ -17,18 +19,19 @@ interface Props {
 const WordCardReviewMode: FC<Props> = ({ word }) => {
   const { id } = word
   const [isPeekMode, setPeekMode] = useState(false)
-  const setSelectedWordIdForDialog = useSetRecoilState(
-    selectedWordIdForDialogState,
-  )
 
-  const onClickOpenEditDialog = useCallback(() => {
-    setSelectedWordIdForDialog(id)
-  }, [id, setSelectedWordIdForDialog])
+  const onClickWordCard = useRecoilCallback(
+    ({ set }) =>
+      () => {
+        set(selectedWordIdForDialogState, id)
+      },
+    [id],
+  )
 
   return (
     <StyledSuspense>
       <Card style={{ width: `100%`, borderRadius: 9 }}>
-        <CardContent>
+        <CardContent onClick={onClickWordCard}>
           <Typography variant="h5" component="div">
             {isPeekMode ? word.term : `???`}
           </Typography>
@@ -46,13 +49,17 @@ const WordCardReviewMode: FC<Props> = ({ word }) => {
           <StyledVisibilityAtom
             isVisible={!isPeekMode}
             onClick={() => setPeekMode(!isPeekMode)}
-            visibleHoverMessage={`Peek this Wordcard`}
+            visibleHoverMessage={`Peek this word card`}
           />
-          <StyledIconButtonAtom
-            jsxElementButton={<EditIcon fontSize="small" />}
-            onClick={onClickOpenEditDialog}
-          />
+          {isPeekMode && !word.isArchived && (
+            <WordCardArchiveButtonPart wordId={word.id} />
+          )}
+          {isPeekMode && word.isArchived && (
+            <WordCardUnarchiveButtonPart wordId={word.id} />
+          )}
+          {isPeekMode && <WordCardShareButtonPart wordId={word.id} />}
           <TagButtonChunk wordId={word.id} />
+          <DictLinkButtonChunk wordId={word.id} />
         </CardActions>
       </Card>
     </StyledSuspense>
