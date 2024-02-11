b110dd270624fd79b697882d6145d108a620b49c
AJ Kim <jkim67cloud@gmail.com>
2024-02-11 14:12:45 +0900

    feat: loading button smartly moves

--- src/components/atom_appbar_parts/index.to_consistency_page_button.tsx
+++ src/components/atom_appbar_parts/index.to_consistency_page_button.tsx
@@ -2,18 +2,24 @@ import { FC } from 'react'
 import StyledTextButtonAtom from '@/atoms/StyledTextButton'
 import { useOpenNewTab } from '@/hooks/use-open-new-tab'
 import { envLambda } from '@/lambdas/get-env.lambda'
+import { useRecoilValue } from 'recoil'
+import { isWordPostedDailyState } from '@/recoil/action-groups/action-groups.state'
 
 /** Simply renders a message that the end user is using archive mode.
  * Else, simply returns null, representing it is using normal mode.
  */
 const AppbarToConsistencyPageButtonPart: FC = () => {
   const consistencyPage = envLambda.getConsistencyUrl()
+  const isWordPostedDaily = useRecoilValue(isWordPostedDailyState)
   const onOpenNewTab = useOpenNewTab(consistencyPage)
+
+  if (isWordPostedDaily === null) return null
 
   return (
     <StyledTextButtonAtom
-      title="Go to Consistency Page"
+      title={isWordPostedDaily ? `Well done!` : `Go add word please`}
       onClick={onOpenNewTab}
+      isLoading={isWordPostedDaily === undefined}
     />
   )
 }
