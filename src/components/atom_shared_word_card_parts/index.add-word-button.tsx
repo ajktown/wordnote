3b00ac5fe60ff58ddf5a349792aeda159fbe35ef
AJ Kim <jkim67cloud@gmail.com>
2024-02-28 06:58:59 +0900

    feat: post word

--- src/components/atom_shared_word_card_parts/index.add-word-button.tsx
+++ src/components/atom_shared_word_card_parts/index.add-word-button.tsx
@@ -1,12 +1,52 @@
 import StyledTextButtonAtom from '@/atoms/StyledTextButton'
-import { FC } from 'react'
+import { usePostWord } from '@/hooks/words/use-post-word.hook'
+import { sharedWordFamily } from '@/recoil/shared-resource/shared-resource.state'
+import { FC, useCallback, useState } from 'react'
+import { useRecoilValue } from 'recoil'
 
 interface Props {
   wordId: string
 }
 const SharedWordCardAddWordButtonPart: FC<Props> = ({ wordId }) => {
+  const sharedWord = useRecoilValue(sharedWordFamily(wordId))
+  const onPostWord = usePostWord()
+  const [loading, setLoading] = useState(false)
+  const [isAdded, setAdded] = useState(false)
+
+  const onClick = useCallback(async () => {
+    try {
+      if (!sharedWord) return
+      if (!sharedWord.word) return
+
+      setLoading(true)
+      await onPostWord({
+        isFavorite: false,
+        isArchived: false,
+        term: sharedWord.word.term,
+        pronunciation: sharedWord.word.pronunciation,
+        definition: sharedWord.word.definition,
+        example: sharedWord.word?.example,
+        exampleLink: ``,
+        tags: [],
+      })
+
+      // since it is added, it should be already added.
+      setAdded(true)
+    } finally {
+      setLoading(false)
+    }
+  }, [sharedWord, onPostWord])
+
+  if (isAdded)
+    return <StyledTextButtonAtom isDisabled title={`Already added`} />
+
   return (
-    <StyledTextButtonAtom title={`Add this word into your list` + wordId} />
+    <StyledTextButtonAtom
+      onClick={onClick}
+      isLoading={loading}
+      isDisabled={!sharedWord?.word}
+      title={`Add this word into your list`}
+    />
   )
 }
 
