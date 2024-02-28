2ecf8b4dcf6559e07c17a426623f02fd8596100c
Aaron Jeongwoo Kim <53258958+mlajkim@users.noreply.github.com>
2024-02-28 23:40:55 +0900

    Shared Word Page now let users to download the word if signed in (#191)
    # Background
    Implement a button for the shared page to add a certain word right away
    
    If Not Signed in:
    
    ![image](https://github.com/ajktown/wordnote/assets/53258958/5da8883e-55a9-456b-b8de-bd42534deeb6)
    
    If Signed in:
    
    ![image](https://github.com/ajktown/wordnote/assets/53258958/aeff89ee-321f-48c8-ae47-6e9020370c99)
    
    If signed in and already added:
    
    ![image](https://github.com/ajktown/wordnote/assets/53258958/21f54a1b-26eb-47f5-8a91-e3ddd8d05658)
    
    
    
    ## TODOs
    - [x] If signed in: User can click a button to copy the word
    - [x] If not signed in: the post button is disabled and tell that you
    are not signed in
    
    ## Checklist (Developers)
    - [x] `Draft` is set for this PR
    - [x] `Title` is checked
    - [x] `Background` is filled
    - [x] `TODOs` are filled
    - [x] `Assignee` is set
    - [x] `Labels` are set
    - [x] `Project` is created & linked, if multiple PRs are associated to
    this PR
    - [x] `development` is linked if related issue exists
    - [x] `yarn inspect` is run
    - [x] `TODOs` are handled and checked
    - [x] Final Operation Check is done
    - [x] Make this PR as an open PR
    
    ## Checklist (Reviewers)
    - [x] Check if there are any other missing TODOs that are not yet listed
    - [x] Review Code
    - [x] Every item on the checklist has been addressed accordingly
    - [x] If `development` is associated to this PR, you must check if every
    TODOs are handled

---
+++ src/components/atom_shared_word_card_parts/index.add-word-button.tsx
@@ -0,0 +1,68 @@
+import StyledIconButtonNewPage from '@/atoms/StyledIconButtonNewPage'
+import StyledTextButtonAtom from '@/atoms/StyledTextButton'
+import { PageConst } from '@/constants/pages.constant'
+import { usePostWord } from '@/hooks/words/use-post-word.hook'
+import { isSignedInSelector } from '@/recoil/app/app.selectors'
+import { sharedWordFamily } from '@/recoil/shared-resource/shared-resource.state'
+import { Stack } from '@mui/material'
+import { FC, useCallback, useState } from 'react'
+import { useRecoilValue } from 'recoil'
+
+interface Props {
+  wordId: string
+}
+const SharedWordCardAddWordButtonPart: FC<Props> = ({ wordId }) => {
+  const sharedWord = useRecoilValue(sharedWordFamily(wordId))
+  const [loading, onPostWord] = usePostWord()
+  const [isAdded, setAdded] = useState(false)
+  const isSignedIn = useRecoilValue(isSignedInSelector)
+
+  const onClick = useCallback(async () => {
+    try {
+      if (!sharedWord) return
+      if (!sharedWord.word) return
+
+      await onPostWord({
+        isFavorite: false,
+        isArchived: false,
+        term: sharedWord.word.term,
+        pronunciation: sharedWord.word.pronunciation,
+        definition: sharedWord.word.definition,
+        example: sharedWord.word?.example,
+        exampleLink: ``,
+        tags: [],
+      })
+
+      // since it is added, it should be already added.
+      setAdded(true)
+    } catch {}
+  }, [sharedWord, onPostWord])
+
+  if (!isSignedIn)
+    return (
+      <Stack alignItems="center" direction="row">
+        <StyledTextButtonAtom
+          isDisabled
+          title={`Sign in to add this word in your list`}
+        />
+        <StyledIconButtonNewPage
+          link={PageConst.Welcome}
+          hoverMessage="To sign in page"
+        />
+      </Stack>
+    )
+
+  if (isAdded)
+    return <StyledTextButtonAtom isDisabled title={`Already added`} />
+
+  return (
+    <StyledTextButtonAtom
+      onClick={onClick}
+      isLoading={loading}
+      isDisabled={!sharedWord?.word}
+      title={`Add this word into your list`}
+    />
+  )
+}
+
+export default SharedWordCardAddWordButtonPart
