c0cdca86b466e6bf5511f4f59bb7b7f67d986b07
AJ Kim <jkim67cloud@gmail.com>
2024-03-20 17:57:29 +0900

    feat: todo handled

--- src/components/organism_appbar/index.tsx
+++ src/components/organism_appbar/index.tsx
@@ -1,17 +1,72 @@
 import { FC } from 'react'
-import StyledAppbarMolecule from '../../molecules/StyledAppbar'
+import { AppBar, Box } from '@mui/material'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import AppsIcon from '@mui/icons-material/Apps'
+import StyledIconButtonAtom from '@/atoms/StyledIconButton'
+import AppbarSearchBar from '@/components/molecule_appbar_search_bar'
+import Image from 'next/image'
+import EndUserAvatar from '@/components/atom_user_avatar/index.end-user'
+import AppbarToConsistencyPageButtonPart from '@/components/atom_appbar_parts/index.to_consistency_page_button'
+import AppbarGitHubButtonPart from '@/components/atom_appbar_parts/index.github_icon'
+import { useWindowSize } from 'react-use'
 
+const PRIVATE_TITLE = `Wordnote GPT (Beta)`
+const PRIVATE_SHORTER_TITLE = `WGT`
+const PRIVATE_TITLE_SHRINK_PIXELS = 1200
+const PRIVATE_TITLE_LOGO = `/favicon_archived/android-chrome-512x512.png`
 interface Props {
   children?: JSX.Element | JSX.Element[]
 }
-const Appbar: FC<Props> = ({ children }) => {
+const Appbar: FC<Props> = (props) => {
+  const { width } = useWindowSize()
+
   return (
-    <StyledAppbarMolecule
-      title="Wordnote (Beta)"
-      titleLogoPath="/favicon_archived/android-chrome-512x512.png"
-    >
-      {children}
-    </StyledAppbarMolecule>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="transparent">
+        <Toolbar variant="dense">
+          <StyledIconButtonAtom jsxElementButton={<AppsIcon />} isDisabled />
+          <Box width={10} />
+          <Image
+            src={PRIVATE_TITLE_LOGO}
+            alt="logo"
+            width={30}
+            height={30}
+            style={{ marginRight: 8 }}
+          />
+
+          {width <= PRIVATE_TITLE_SHRINK_PIXELS && (
+            <Typography
+              variant="h6"
+              color="inherit"
+              component="div"
+              fontFamily={`Cormorant Garamond`}
+            >
+              {PRIVATE_SHORTER_TITLE}
+            </Typography>
+          )}
+          {PRIVATE_TITLE_SHRINK_PIXELS < width && (
+            <Typography
+              variant="h6"
+              color="inherit"
+              component="div"
+              fontFamily={`Cormorant Garamond`}
+            >
+              {PRIVATE_TITLE}
+            </Typography>
+          )}
+          <Box pr={2} />
+          <AppbarSearchBar />
+          <Box flexGrow={1} />
+          <AppbarToConsistencyPageButtonPart />
+          <Box pr={0.25} />
+          <AppbarGitHubButtonPart />
+          <Box pr={0.25} />
+          <EndUserAvatar />
+        </Toolbar>
+      </AppBar>
+      <Box height="calc(100vh - 48px)">{props.children}</Box>
+    </Box>
   )
 }
 
