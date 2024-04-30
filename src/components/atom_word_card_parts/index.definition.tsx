58fa40cbb5fea0188049f1e53d7d7f15982c4a57
AJ Kim <jkim67cloud@gmail.com>
2024-04-30 15:07:43 +0900

    style: secondary font

--- src/components/atom_word_card_parts/index.definition.tsx
+++ src/components/atom_word_card_parts/index.definition.tsx
@@ -1,20 +1,22 @@
 import { ISharedWord } from '@/api/words/interfaces'
-import { Typography } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 import { FC } from 'react'
 
+// TODO: I think we can design later phase (for now it works)
 interface Props {
   word: ISharedWord
   hideSubDefinition?: boolean // true: does not display sub definition
 }
 const WordCardDefinitionPart: FC<Props> = ({ word, hideSubDefinition }) => {
   return (
-    <Typography variant="body2">
-      {word.definition}
-      {!hideSubDefinition && word.subDefinition
-        ? ` (${word.subDefinition})`
-        : ``}
-      <br />
-    </Typography>
+    <Stack spacing={0}>
+      <Typography variant="body2">{word.definition}</Typography>
+      {!hideSubDefinition && word.subDefinition && (
+        <Typography variant="body2" color="text.secondary" fontStyle={'italic'}>
+          {word.subDefinition}
+        </Typography>
+      )}
+    </Stack>
   )
 }
 
