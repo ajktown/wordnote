f6ade3cb5420c8c011072aba7902a0faf56b768f
AJ Kim <jkim67cloud@gmail.com>
2024-05-02 10:00:36 +0900

    feat: show gpt key

--- src/components/molecule_gpt_key_registerer/index.tsx
+++ src/components/molecule_gpt_key_registerer/index.tsx
@@ -1,13 +1,39 @@
+import StyledIconButtonAtom from '@/atoms/StyledIconButton'
+import StyledTextField from '@/atoms/StyledTextField'
 import { gptApiKeySelector } from '@/recoil/preferences/preferenece.selector'
 import { Alert, Typography } from '@mui/material'
-import { FC } from 'react'
+import { FC, Fragment, useCallback, useState } from 'react'
 import { useRecoilValue } from 'recoil'
+import CheckIcon from '@mui/icons-material/Check'
+import ClearIcon from '@mui/icons-material/Clear'
 
 const GptKeyRegisterer: FC = () => {
   const gptApiKey = useRecoilValue(gptApiKeySelector)
+  const [input, setInput] = useState<string>(gptApiKey)
+
+  const onClickApply = useCallback(() => {}, [])
+  const onClickCancel = useCallback(() => setInput(gptApiKey), [gptApiKey])
 
   return (
     <Alert severity="info">
+      <StyledTextField
+        value={input}
+        onChange={setInput}
+        buttons={{
+          right: input != gptApiKey && (
+            <Fragment>
+              <StyledIconButtonAtom
+                jsxElementButton={<CheckIcon fontSize="small" />}
+                onClick={onClickApply}
+              />
+              <StyledIconButtonAtom
+                jsxElementButton={<ClearIcon fontSize="small" />}
+                onClick={onClickCancel}
+              />
+            </Fragment>
+          ),
+        }}
+      />
       <Typography variant="caption">{gptApiKey}</Typography>
     </Alert>
   )
