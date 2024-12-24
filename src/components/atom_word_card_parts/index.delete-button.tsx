3ecc043cefd2172cbccf7c24a6e8c27957a97f59
AJ Kim <jkim67cloud@gmail.com>
2024-12-25 05:35:00 +0900

    feat: demo

--- src/components/atom_word_card_parts/index.delete-button.tsx
+++ src/components/atom_word_card_parts/index.delete-button.tsx
@@ -1,18 +1,34 @@
 import StyledIconButtonAtom from '@/atoms/StyledIconButton'
-import { FC } from 'react'
+import { FC, Fragment, useCallback, useState } from 'react'
 import DeleteWordIcon from '@mui/icons-material/Delete'
 import { useDeleteWord } from '@/hooks/words/use-delete-word.hook'
+import StyledTextButtonAtom from '@/atoms/StyledTextButton'
 interface Props {
   wordId: string
 }
 const WordCardDeleteButtonPart: FC<Props> = ({ wordId }) => {
+  const [isDoubleChecking, setDoubleChecking] = useState(false)
   const [isDeleting, onDeleteWord] = useDeleteWord(wordId)
+
+  const onClick = useCallback(() => {
+    setDoubleChecking(!isDoubleChecking)
+  }, [isDoubleChecking])
+
+  // TODO: Use memo
+  const jsxElementButton = isDoubleChecking ? (
+    <Fragment>
+      <StyledTextButtonAtom title={`Delete!`} onClick={onDeleteWord} />
+      <StyledTextButtonAtom title={`No!`} onClick={onClick} />
+    </Fragment>
+  ) : (
+    <DeleteWordIcon />
+  )
 
   return (
     <StyledIconButtonAtom
       isDisabled={isDeleting}
-      onClick={onDeleteWord}
-      jsxElementButton={<DeleteWordIcon />}
+      onClick={onClick}
+      jsxElementButton={jsxElementButton}
     />
   )
 }
