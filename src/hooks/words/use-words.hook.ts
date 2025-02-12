0dbc4a39eeec34e4520a86ce7d73c886f111f7a1
AJ Kim <jkim67cloud@gmail.com>
2025-02-12 09:11:28 +0900

    refactor: with mode

--- src/hooks/words/use-words.hook.ts
+++ src/hooks/words/use-words.hook.ts
@@ -9,7 +9,7 @@ import { useState } from 'react'
 import { useRecoilCallback } from 'recoil'
 import { useHandleApiError } from '../use-handle-api-error.hook'
 import { useApplySemesterDetails } from '../semesters/use-apply-semester-details'
-import { GetWordsApi, getWordsApi } from '@/api/words/get-words.api'
+import { getWordsApi } from '@/api/words/get-words.api'
 import { isEveryFavoriteSelectedState } from '@/recoil/words/semesters.state'
 
 type NewParams = Partial<GetWordParams>
@@ -18,6 +18,20 @@ type HandleRefresh = (
   isEveryFavoriteSelected?: boolean,
 ) => Promise<void>
 type UseWordIds = [boolean, HandleRefresh]
+
+type PrivateMode =
+  | 'default-mode' // the default page mode that you see when you open up the page
+  | 'search-mode' // when you search for a word in a search bar
+  | 'every-favorite-mode' // when you click on the every-favorite button
+// getMode returns the WordNote's viewing mode defined in "PrivateMode"
+const getMode = (
+  newParams: Partial<GetWordParams> | undefined,
+  isEveryFavoriteSelected?: boolean,
+): PrivateMode => {
+  if (newParams && newParams.searchInput?.trim()) return `search-mode`
+  if (isEveryFavoriteSelected) return `every-favorite-mode`
+  else return `default-mode`
+}
 
 export const useWords = (): UseWordIds => {
   const [loading, setLoading] = useState(false)
@@ -35,32 +49,42 @@ export const useWords = (): UseWordIds => {
           // Prepare params:
           let params: Partial<GetWordParams> | undefined = undefined
           let updateParams = false
-          if (newParams && newParams.searchInput?.trim()) {
-            params = { searchInput: newParams.searchInput }
-          } else if (
-            // The reason why it checks both isEveryFavoriteSelectedState and isEveryFavoriteSelected
-            // is because the state isEveryFavoriteSelectedState is not updated immediately
-            // To make sure it is updated, it checks both If one of them is true, it means it is in every-favorite mode
-            (await snapshot.getPromise(isEveryFavoriteSelectedState)) ||
-            isEveryFavoriteSelected
+          switch (
+            getMode(
+              newParams, // The reason why it checks both isEveryFavoriteSelectedState and isEveryFavoriteSelected
+              // is because the state isEveryFavoriteSelectedState is not updated immediately
+              // To make sure it is updated, it checks both If one of them is true, it means it is in every-favorite mode
+              (await snapshot.getPromise(isEveryFavoriteSelectedState)) ||
+                isEveryFavoriteSelected,
+            )
           ) {
-            params = { isFavorite: true, pageIndex: newParams?.pageIndex }
-          } else {
-            params = {
-              ...(await snapshot.getPromise(getWordsParamsState)),
-              ...newParams,
-            }
-            // only stores in cache if it is "normal-mode" excluding:
-            // - every-favorite mode
-            // - search mode
-            updateParams = true
+            case `search-mode`:
+              params = {
+                searchInput: newParams?.searchInput,
+                pageIndex: newParams?.pageIndex,
+              }
+              break
+            case `every-favorite-mode`:
+              params = { isFavorite: true, pageIndex: newParams?.pageIndex }
+              break
+
+            case `default-mode`:
+            default:
+              params = {
+                ...(await snapshot.getPromise(getWordsParamsState)),
+                ...newParams,
+              }
+              // only stores in cache if it is "normal-mode" excluding:
+              // - every-favorite mode
+              // - search mode
+              updateParams = true
+              break
           }
+
           if (updateParams) set(getWordsParamsState, params)
 
           // Get words from API:
-          const apiResponse: undefined | GetWordsApi = (
-            await getWordsApi(params)
-          )[0]
+          const apiResponse = (await getWordsApi(params))[0]
           if (!apiResponse) throw new Error(`apiResponse is undefined`)
 
           apiResponse.words.forEach((word) => {
