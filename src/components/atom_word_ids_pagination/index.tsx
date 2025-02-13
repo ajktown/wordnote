6453be17ec6c3d9f6ae5dc9f73ee176cd6c4a5c5
Aaron Jeongwoo Kim <53258958+mlajkim@users.noreply.github.com>
2025-02-14 08:31:47 +0900

    Bugfix: `SearchMode` / `FavoriteOnlyMode` / `DefaultMode` switches smoothly now (#242)
    # Background
    WGT has three stages defined in `PrivateMode`:
    - DefaultMode: The default `semester` clicked page
    - SearchMode: When user wants to search in search: bar
    - FavoriteOnlyMode: When the favorite only is clicked
    
    Switching between modes have some bugs.
    
    ## What's done
    Fixes the bug described in the `Background`
    
    ## What are the related PRs?
    - N/A
    
    ## Checklist Before PR Review
    - [x] The following has been handled:
      - `Draft` is set for this PR
      - `Title` is checked
      - `Background` is filled
      - `Assignee` is set
      - `Labels` are set
      - `Development` is linked if related issue exists
    
    ## Checklist (Right Before PR Review Request)
    - [x] The following has been handled:
      - Final Operation Check is done
      - Mobile View Operation Check is done
      - Make this PR as an open PR
      - `What's done` is filled & matches to the actual changes
    
    ## Checklist (Reviewers)
    - [x] Review if the following has been handled:
      - Review Code
      - `Checklist Before PR Review` is correctly handled
      - `Checklist (Right Before PR Review Request)` is correctly handled
    - Check if there are any other missing TODOs (Checklists) that are not
    yet listed
      - Check if issue under `Development` is fully handled if exists

--- src/components/atom_word_ids_pagination/index.tsx
+++ src/components/atom_word_ids_pagination/index.tsx
@@ -1,25 +1,20 @@
-import { useWordsByFavorite } from '@/hooks/words/use-words-by-favorite.hook'
 import { useWords } from '@/hooks/words/use-words.hook'
 import StyledPaginatorMolecule from '@/molecules/StyledPaginator'
-import { isEveryFavoriteSelectedState } from '@/recoil/words/semesters.state'
 import { wordIdsPagination } from '@/recoil/words/words.state'
 import { FC, useCallback } from 'react'
 import { useRecoilValue } from 'recoil'
 
 const WordIdsPagination: FC = () => {
-  const isEveryFavoriteSelected = useRecoilValue(isEveryFavoriteSelectedState)
   const pagination = useRecoilValue(wordIdsPagination)
 
   const [, onGetWords] = useWords()
-  const [, onGetWordsByFavorite] = useWordsByFavorite()
 
   const onChange = useCallback(
     async (newPage: number) => {
       window.scrollTo(0, 0)
-      if (isEveryFavoriteSelected) await onGetWordsByFavorite(newPage - 1)
-      else await onGetWords({ pageIndex: newPage - 1 })
+      onGetWords({ pageIndex: newPage - 1 })
     },
-    [isEveryFavoriteSelected, onGetWords, onGetWordsByFavorite],
+    [onGetWords],
   )
 
   if (!pagination) return null
--- src/hooks/words/use-words.hook.ts
+++ src/hooks/words/use-words.hook.ts
@@ -9,37 +9,81 @@ import { useState } from 'react'
 import { useRecoilCallback } from 'recoil'
 import { useHandleApiError } from '../use-handle-api-error.hook'
 import { useApplySemesterDetails } from '../semesters/use-apply-semester-details'
-import { GetWordsApi, getWordsApi } from '@/api/words/get-words.api'
+import { getWordsApi } from '@/api/words/get-words.api'
+import { isEveryFavoriteSelectedState } from '@/recoil/words/semesters.state'
 
 type NewParams = Partial<GetWordParams>
-type HandleRefresh = (newParams?: NewParams) => Promise<void>
+type HandleRefresh = (
+  newParams?: NewParams,
+  isEveryFavoriteSelected?: boolean,
+) => Promise<void>
 type UseWordIds = [boolean, HandleRefresh]
+
+// TODO: Write the mode in a separate file (maybe const) because this contains AppConstLogic
+type PrivateMode =
+  | 'default-mode' // the default page mode that you see when you open up the page
+  | 'search-mode' // when you search for a word in a search bar
+  | 'every-favorite-mode' // when you click on the every-favorite button
+// getMode returns the WordNote's viewing mode defined in "PrivateMode"
+const getMode = (
+  newParams: Partial<GetWordParams> | undefined,
+  isEveryFavoriteSelected?: boolean,
+): PrivateMode => {
+  if (newParams && newParams.searchInput?.trim()) return `search-mode`
+  if (isEveryFavoriteSelected) return `every-favorite-mode`
+  else return `default-mode`
+}
 
 export const useWords = (): UseWordIds => {
   const [loading, setLoading] = useState(false)
   const onHandleApiError = useHandleApiError()
   const onApplySemesterDetails = useApplySemesterDetails()
 
+  // onGetWords smartly handles each mode defined in PrivateMode and prepare a proper query against the API:
   const onGetWords: HandleRefresh = useRecoilCallback(
     ({ set, reset, snapshot }) =>
-      async (newParams?: NewParams) => {
+      async (newParams?: NewParams, isEveryFavoriteSelected?: boolean) => {
         setLoading(true)
         try {
-          const params: Partial<GetWordParams> = {
-            ...(await snapshot.getPromise(getWordsParamsState)),
-            ...newParams,
-          }
-          set(getWordsParamsState, params)
+          // Prepare params:
+          let params: Partial<GetWordParams> | undefined = undefined
+          let updateParams = false
+          switch (
+            getMode(
+              newParams, // The reason why it checks both isEveryFavoriteSelectedState and isEveryFavoriteSelected
+              // is because the state isEveryFavoriteSelectedState is not updated immediately
+              // To make sure it is updated, it checks both If one of them is true, it means it is in every-favorite mode
+              (await snapshot.getPromise(isEveryFavoriteSelectedState)) ||
+                isEveryFavoriteSelected,
+            )
+          ) {
+            case `search-mode`:
+              params = {
+                searchInput: newParams?.searchInput,
+                pageIndex: newParams?.pageIndex,
+              }
+              break
+            case `every-favorite-mode`:
+              params = { isFavorite: true, pageIndex: newParams?.pageIndex }
+              break
 
-          let apiResponse: undefined | GetWordsApi = undefined
-          if (params.searchInput?.trim()) {
-            // if search input is given, then we should get all words from every semester
-            apiResponse = (
-              await getWordsApi({ searchInput: params.searchInput })
-            )[0]
-          } else {
-            apiResponse = (await getWordsApi(params))[0]
+            case `default-mode`:
+            default:
+              params = {
+                ...(await snapshot.getPromise(getWordsParamsState)),
+                ...newParams,
+              }
+              // only stores in cache if it is "normal-mode" excluding:
+              // - every-favorite mode
+              // - search mode
+              updateParams = true
+              break
           }
+
+          if (updateParams) set(getWordsParamsState, params)
+
+          // Get words from API:
+          const apiResponse = (await getWordsApi(params))[0]
           if (!apiResponse) throw new Error(`apiResponse is undefined`)
 
           apiResponse.words.forEach((word) => {
